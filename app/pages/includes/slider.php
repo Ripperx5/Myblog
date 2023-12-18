aa8a2a644fb9d75a13d9d8eb11231decb3b544af
Sipun Gupta <sipungupta2311@gmail.com>
2023-12-18 21:59:25 +0530

    final commit

---
+++ app/pages/includes/slider.php
@@ -0,0 +1,26 @@
+
+  <!-- slider -->
+  <link rel="stylesheet" href="<?=ROOT?>/assets/slider/ism/css/my-slider.css"/>
+  <script src="<?=ROOT?>/assets/slider/ism/js/ism-2.2.min.js"></script>
+
+  <div class="ism-slider" data-transition_type="fade" data-play_type="loop" id="my-slider">
+    <ol>
+      <li>
+        <img src="<?=ROOT?>/assets/images/1.jpg">
+        <div class="ism-caption ism-caption-0">Blogging is good for your career. A well-executed blog sets you apart as an expert in your field.</div>
+      </li>
+      <li>
+        <img src="<?=ROOT?>/assets/slider/ism/image/slides/beautiful-701678_1280.jpg">
+        <div class="ism-caption ism-caption-0">My informal writing style is a political choice, because I want feminism to be more accessible.</div>
+      </li>
+      <li>
+        <img src="<?=ROOT?>/assets/slider/ism/image/slides/summer-192179_1280.jpg">
+        <div class="ism-caption ism-caption-0">The first step in blogging is not writing them but reading them.</div>
+      </li>
+      <li>
+        <img src="<?=ROOT?>/assets/slider/ism/image/slides/city-690332_1280.jpg">
+        <div class="ism-caption ism-caption-0">The best time to act on this was decades ago. The second best time is now.</div>
+      </li>
+    </ol>
+  </div>
+  <!-- end slider -->
